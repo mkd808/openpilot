7739e8e3ed5df31f852a59a1d671338eb5a8d6e7
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-01-19 17:32:31 -0700

    Use EV gas lookup tables
    Co-Authored-By: Tim Wilson <7284371+twilsonco@users.noreply.github.com>
    Co-Authored-By: Eric Brown <13560103+nworb-cire@users.noreply.github.com>

--- selfdrive/frogpilot/ui/vehicle_settings.cc
+++ selfdrive/frogpilot/ui/vehicle_settings.cc
@@ -99,6 +99,7 @@ FrogPilotVehiclesPanel::FrogPilotVehiclesPanel(SettingsWindow *parent) : FrogPil
   selectModelButton->setVisible(false);
 
   std::vector<std::tuple<QString, QString, QString, QString>> vehicleToggles {
+    {"EVTable", "EV Lookup Tables", "Smoothen out the gas and brake controls for EV vehicles.", ""},
     {"GasRegenCmd", "GM Truck Gas Tune", "Increase acceleration and smoothen brake to stop. For use on Silverado/Sierra only.", ""},
     {"LongPitch", "Long Pitch Compensation", "Reduce speed and acceleration error for greater passenger comfort and improved vehicle efficiency.", ""},
     {"LowerVolt", "Lower Volt Enable Speed", "Lower the Volt's minimum enable speed to enable openpilot at any speed.", ""},
@@ -120,7 +121,7 @@ FrogPilotVehiclesPanel::FrogPilotVehiclesPanel(SettingsWindow *parent) : FrogPil
     });
   }
 
-  gmKeys = {"GasRegenCmd", "LongPitch", "LowerVolt"};
+  gmKeys = {"EVTable", "GasRegenCmd", "LongPitch", "LowerVolt"};
   toyotaKeys = {"LockDoors", "SNGHack", "TSS2Tune"};
 
   std::set<std::string> rebootKeys = {"GasRegenCmd", "LongPitch", "LowerVolt", "TSS2Tune"};
