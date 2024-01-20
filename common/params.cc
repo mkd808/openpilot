6a0a57fa1c026ee9e85ea03c018b2d7bca9f959e
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-01-19 17:32:31 -0700

    Force fingerprint function
    Added function to automatically detect the vehicle's fingerprint if it exists, or an option to force the vehicle's fingerprint if it doesn't.

--- common/params.cc
+++ common/params.cc
@@ -225,6 +225,8 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"AverageCurvature", PERSISTENT},
     {"BlindSpotPath", PERSISTENT},
     {"CameraView", PERSISTENT},
+    {"CarMake", PERSISTENT},
+    {"CarModel", PERSISTENT},
     {"CECurves", PERSISTENT},
     {"CECurvesLead", PERSISTENT},
     {"CENavigation", PERSISTENT},
