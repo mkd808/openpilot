0f905e9eaf8c9090f258de742825c9b0137f5ac3
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-01-19 17:32:31 -0700

    Calculate average desired curvature using planned distance
    Added toggle to calculate average desired curvature using planned distance.
    
    Credit goes to Pfeiferj!
    
    https: //github.com/pfeiferj
    Co-Authored-By: Jacob Pfeifer <jacob@pfeifer.dev>

--- selfdrive/frogpilot/functions/frogpilot_planner.py
+++ selfdrive/frogpilot/functions/frogpilot_planner.py
@@ -1,14 +1,20 @@
 import cereal.messaging as messaging
+import numpy as np
 
 from openpilot.common.conversions import Conversions as CV
-from openpilot.selfdrive.controls.lib.drive_helpers import V_CRUISE_MAX
+from openpilot.common.numpy_fast import interp
+from openpilot.selfdrive.controls.lib.drive_helpers import CONTROL_N, V_CRUISE_MAX
+from openpilot.selfdrive.controls.lib.longitudinal_mpc_lib.long_mpc import T_IDXS as T_IDXS_MPC
 from openpilot.selfdrive.controls.lib.longitudinal_planner import A_CRUISE_MIN, A_CRUISE_MAX_BP, get_max_accel
+from openpilot.selfdrive.modeld.constants import ModelConstants
 
 from openpilot.selfdrive.frogpilot.functions.frogpilot_functions import get_min_accel_eco, get_max_accel_eco, get_min_accel_sport, get_max_accel_sport
 
 class FrogPilotPlanner:
   def __init__(self, params, params_memory):
     self.v_cruise = 0
+
+    self.x_desired_trajectory = np.zeros(CONTROL_N)
 
     self.update_frogpilot_params(params, params_memory)
 
@@ -31,6 +37,9 @@ class FrogPilotPlanner:
 
     self.v_cruise = self.update_v_cruise(carState, controlsState, modelData, enabled, v_cruise, v_ego)
 
+    self.x_desired_trajectory_full = np.interp(ModelConstants.T_IDXS, T_IDXS_MPC, mpc.x_solution)
+    self.x_desired_trajectory = self.x_desired_trajectory_full[:CONTROL_N]
+
   def update_v_cruise(self, carState, controlsState, modelData, enabled, v_cruise, v_ego):
     v_ego_diff = max(carState.vEgoRaw - carState.vEgoCluster, 0)
     return v_cruise - v_ego_diff
@@ -47,6 +56,8 @@ class FrogPilotPlanner:
     frogpilot_longitudinal_plan_send.valid = sm.all_checks(service_list=['carState', 'controlsState'])
     frogpilotLongitudinalPlan = frogpilot_longitudinal_plan_send.frogpilotLongitudinalPlan
 
+    frogpilotLongitudinalPlan.distances = self.x_desired_trajectory.tolist()
+
     pm.send('frogpilotLongitudinalPlan', frogpilot_longitudinal_plan_send)
 
   def update_frogpilot_params(self, params, params_memory):
@@ -56,6 +67,7 @@ class FrogPilotPlanner:
     self.blind_spot_path = params.get_bool("BlindSpotPath") and custom_ui
 
     lateral_tune = params.get_bool("LateralTune")
+    self.average_desired_curvature = params.get_bool("AverageCurvature") and lateral_tune
 
     longitudinal_tune = params.get_bool("LongitudinalTune")
     self.acceleration_profile = params.get_int("AccelerationProfile") if longitudinal_tune else 0
