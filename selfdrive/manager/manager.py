ab74267b16408f0263a7b4e22705a95d83dd3149
FrogAi <91348155+FrogAi@users.noreply.github.com>
2024-01-19 17:32:31 -0700

    Sentry logging
    Logging for my Sentry server that tracks the values of the serial number, car fingerprint, user set parameters, and the date and time for when FrogPilot was installed and last updated for debugging and support.

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -33,6 +33,10 @@ def manager_init() -> None:
   # save boot log
   subprocess.call("./bootlog", cwd=os.path.join(BASEDIR, "system/loggerd"))
 
+  # Clear the error log on boot to prevent old errors from hanging around
+  if os.path.isfile(os.path.join(sentry.CRASHES_DIR, 'error.txt')):
+    os.remove(os.path.join(sentry.CRASHES_DIR, 'error.txt'))
+
   params = Params()
   params.clear_all(ParamKeyType.CLEAR_ON_MANAGER_START)
   params.clear_all(ParamKeyType.CLEAR_ON_ONROAD_TRANSITION)
